4c84532dffc6f092ab577c5d5f3ff89b25dbbdba
Frederich Blessy <frederichblessy@gmail.com>
2024-05-29 23:10:41 +0700

    feature: analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
-import { Route, Routes } from 'react-router-dom';
-import { Box } from '@mantine/core'
+import { Route, Routes } from "react-router-dom";
+import { Box } from "@mantine/core";
+import { Analytics } from "@vercel/analytics/react";
 
-import Home from './pages/Home';
-import CustomHeader from './components/CustomHeader';
-import Project from './pages/Project';
+import Home from "./pages/Home";
+import CustomHeader from "./components/CustomHeader";
+import Project from "./pages/Project";
 
 function App() {
   return (
     <>
-      <Box style={{ position: 'relative', width: '100%', height: '100vh' }}>
+      <Box style={{ position: "relative", width: "100%", height: "100vh" }}>
         <CustomHeader />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Project />} />
         </Routes>
-
-      </Box >
+        <Analytics />
+      </Box>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
