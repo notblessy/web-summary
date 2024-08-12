8d699b9aecc606f5ffb20ad9b0ef1d64f0cfee21
Frederich Blessy <frederichblessy@gmail.com>
2024-08-12 23:15:43 +0700

    add bagirata

--- src/pages/Project/projects.js
+++ src/pages/Project/projects.js
@@ -1,74 +1,91 @@
 export const projects = [
   {
+    id: "bagirata",
+    name: "Bagirata",
+    label: "bagirata",
+    description: "Personal iOS APP that help you split bills with friends",
+    picture: "/bag.png",
+    link: "https://apps.apple.com/id/app/bagirata/id6572284039",
+  },
+  {
     id: "dealls",
     name: "Dealls",
     label: "Dealls",
-    description: "Develop & maintain the Employer Dashboard of Dealls as a Indonesia's #1 Jobs & Mentoring Platform and some another products",
+    description:
+      "Develop & maintain the Employer Dashboard of Dealls as a Indonesia's #1 Jobs & Mentoring Platform and some another products",
     picture: "/de.png",
-    link: "https://dealls.com"
+    link: "https://dealls.com",
   },
   {
     id: "jsonplay",
     name: "JSON Playground",
     label: "Personal",
-    description: "It is your go-to online JSON editing tool that combines error detection, automatic formatting, and a user-friendly design into one seamless experience.",
+    description:
+      "It is your go-to online JSON editing tool that combines error detection, automatic formatting, and a user-friendly design into one seamless experience.",
     picture: "/jsonp.png",
-    link: "https://jsoneditor.pro"
+    link: "https://jsoneditor.pro",
   },
   {
     id: "era",
     name: "Eraspace",
     label: "Erajaya",
-    description: "Working on order service and management in Eraspace. The project is making e-commerce about its product",
+    description:
+      "Working on order service and management in Eraspace. The project is making e-commerce about its product",
     picture: "/era.png",
-    link: "https://eraspace.com"
+    link: "https://eraspace.com",
   },
   {
     id: "ibox",
     name: "iBox",
     label: "Erajaya",
-    description: "Working on order service and management system in iBox as the leading reseller of premium Apple products in Indonesia.",
+    description:
+      "Working on order service and management system in iBox as the leading reseller of premium Apple products in Indonesia.",
     picture: "/ibox.png",
-    link: "https://ibox.co.id"
+    link: "https://ibox.co.id",
   },
   {
     id: "jd",
     name: "JDSport",
     label: "Erajaya",
-    description: "Working on order service and management in system JD Sports as the leading retailer of training footwear products worldwide. As the 'KING OF TRAINERS'.",
+    description:
+      "Working on order service and management in system JD Sports as the leading retailer of training footwear products worldwide. As the 'KING OF TRAINERS'.",
     picture: "/jd.png",
-    link: "https://jdsport.id"
+    link: "https://jdsport.id",
   },
   {
     id: "kumparan",
     name: "kumparan",
     label: "kumparan",
-    description: "Handling all services of kumparan as an Indonesia's Leading Digital News Media Platform with world-class storytelling and journalism. ",
+    description:
+      "Handling all services of kumparan as an Indonesia's Leading Digital News Media Platform with world-class storytelling and journalism. ",
     picture: "/kump.png",
-    link: "https://kumparan.com"
+    link: "https://kumparan.com",
   },
   {
     id: "kontaklink",
     name: "Kontaklink",
     label: "Takalab",
-    description: "kontaklink is from takalab, it's a micro website builder that can easily built from UI and manage it. It also support analytics",
+    description:
+      "kontaklink is from takalab, it's a micro website builder that can easily built from UI and manage it. It also support analytics",
     picture: "/kontak.png",
-    link: "https://kontak.link"
+    link: "https://kontak.link",
   },
   {
     id: "tinggal",
     name: "Tinggal.id",
     label: "Takalab",
-    description: "I was part of this project which is about property listing and management system, designed to help users list and manage it.",
+    description:
+      "I was part of this project which is about property listing and management system, designed to help users list and manage it.",
     picture: "/tinggal.png",
-    link: "https://tinggal.id"
+    link: "https://tinggal.id",
   },
   {
     id: "hellopgmta",
     name: "Hello PGMTA",
     label: "Self Employee",
-    description: "This project is an online store that allows customers to shop online and assists administrators in managing stock efficiently.",
+    description:
+      "This project is an online store that allows customers to shop online and assists administrators in managing stock efficiently.",
     picture: "/hello.png",
-    link: "https://hello-pgmta.com"
-  }
-]
+    link: "https://hello-pgmta.com",
+  },
+];
